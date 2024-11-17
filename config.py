777c879f4a75169aa243e0ca468ba145b9f71f78
Alisher Izmukhan <alisher.izmukhan@nu.edu.kz>
2024-11-18 01:44:00 +0500

    v1.6

--- config.py
+++ config.py
@@ -1,5 +1,9 @@
 import os
+from dotenv import load_dotenv
+
+# Load environment variables from a .env file
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://neondb_owner:xeCPhZXVi84U@ep-divine-leaf-a27a0qkf.eu-central-1.aws.neon.tech/neondb?sslmode=require"
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
