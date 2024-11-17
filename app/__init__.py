398c77bc2a2f88919e4935dddb86cfbe6a451c1e
Alisher Izmukhan <alisher.izmukhan@nu.edu.kz>
2024-11-18 01:33:41 +0500

    v1.5

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 db = SQLAlchemy()
@@ -9,5 +10,6 @@ def create_app():
     app.config.from_object(Config)
 
     db.init_app(app)
+    Migrate(app, db)
 
     return app
