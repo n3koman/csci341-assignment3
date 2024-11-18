56445a9d70bf624067cdfaa30e4b6f86ab597d6d
Alisher Izmukhan <alisher.izmukhan@nu.edu.kz>
2024-11-18 17:35:58 +0500

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,5 @@
 import os
-
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL",
-        "DATABASE_URL=postgresql://neondb_owner:xeCPhZXVi84U@ep-divine-leaf-a27a0qkf.eu-central-1.aws.neon.tech/neondb?sslmode=require",
-    )
+    SQLALCHEMY_DATABASE_URI = 'postgresql://neondb_owner:xeCPhZXVi84U@ep-divine-leaf-a27a0qkf.eu-central-1.aws.neon.tech/neondb?sslmode=require'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
